00f3a73eb7562c98ea216a709b70ac451f9c7b5c
Michael Osborne <resonatortune@gmail.com>
2019-06-04 13:30:49 -0700

    Add CORS proxy url

--- public/races.js
+++ public/races.js
@@ -1,7 +1,9 @@
 class Races {
   constructor() {
+    let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+
     const userAction = async () => {
-      const response = await fetch('https://osbornem-database-project.herokuapp.com/getraces');
+      const response = await fetch(proxyUrl + 'https://osbornem-database-project.herokuapp.com/getraces');
       const myJson = await response.json(); //extract JSON from the http response
       console.log(myJson);
       // do something with myJson
