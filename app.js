286db69fca96d6ba07ca21a4768e857ee4e972e5
Michael Osborne <resonatortune@gmail.com>
2019-06-04 19:49:24 -0700

    Add results endpoint

--- app.js
+++ app.js
@@ -5,12 +5,10 @@ const bodyParser = require('body-parser');
 const dbUtils = require('./utils.js');
 
 const router = express();
-// router.use(bodyParser.json());
-// router.use('/refresh', require('./routes/refresh-database.js'));
 // serve static webpages statically
 router.use(express.static('public'));
 
-// query database endpoints
+// endpoints
 router.get('/getraces', (req, res) => {
   let db = dbUtils.newConnection();
   let options = {
@@ -43,6 +41,23 @@ router.get('/gettracks', (req, res) => {
   let db = dbUtils.newConnection();
   let options = {
     sql: 'SELECT * FROM TRACK',
+  };
+  db.query(options, function(error, results, fields) {
+    res.send(results);
+    db.end();
+  });
+});
+
+router.post('/getraceresults', (req, res) => {
+  let db = dbUtils.newConnection();
+  let options = {
+    sql: 'SELECT DISTINCT * FROM car, `result`, team_car_relationship, team, car_owner_car_relationship, car_owner, manufacturer_car_relationship, manufacturer, driver_car_relationship, driver\
+          WHERE car.CarID = team_car_relationship.CarID AND team.TeamID = team_car_relationship.TeamID\
+          and car.CarID = manufacturer_car_relationship.CarID and manufacturer.ManufacturerID = manufacturer_car_relationship.ManufacturerID\
+          and car.CarID = car_owner_car_relationship.CarID and car_owner_car_relationship.OwnerID = car_owner.OwnerID\
+          and car.CarID = driver_car_relationship.CarID and driver_car_relationship.DriverID = driver.DriverID\
+          and car.CarID = `result`.CarID and `result`.RaceID="' + req.raceID + '";',
+    nestTables: true,
   };
   db.query(options, function(error, results, fields) {
     res.send(results);
--- public/single-race-view.js
+++ public/single-race-view.js
@@ -15,6 +15,25 @@ class SingleRaceView {
     document.getElementById('victoryMargin').innerHTML = data.RACE.VictoryMargin;
     document.getElementById('averageSpeed').innerHTML = data.RACE.AverageSpeed;
     document.getElementById('comments').innerHTML = data.RACE.Comments;
+
+    let proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    fetch(proxyUrl + 'https://osbornem-database-project.herokuapp.com/getraceresults', {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, cors, *same-origin
+      cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'same-origin', // include, *same-origin, omit
+      headers: {
+          'Content-Type': 'application/json',
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrer: 'no-referrer', // no-referrer, *client
+      body: JSON.stringify({
+        raceID: data.RACE.RaceID,
+      }), // body data type must match "Content-Type" header
+    }),then((response) => {
+      console.log(response);
+    })
   }
 }
 
